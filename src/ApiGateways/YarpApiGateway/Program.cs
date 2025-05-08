e6d8ea9fa8108857f63eb08e14ab3e345f9a75e0
mohamed blgikey <a.z29620014@gmail.com>
2025-05-09 01:31:34 +0300

    commit

--- src/ApiGateways/YarpApiGateway/Program.cs
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -1,11 +1,23 @@
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddFixedWindowLimiter("customPolicy", opt =>
+    {
+        opt.PermitLimit = 2;
+        opt.Window = TimeSpan.FromSeconds(12);
+    });
+});
 
 var app = builder.Build();
+app.UseRateLimiter();
+
 app.MapReverseProxy();
 
 app.Run();
